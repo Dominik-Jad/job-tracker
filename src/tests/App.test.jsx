96b86a01b39e9784613152a1d9a80903aae4bcf4
Dominik Jadczak <123301714+Dominik-Jad@users.noreply.github.com>
2024-03-12 20:42:24 +0000

    added testing suite

---
+++ src/tests/App.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+// Test 1: Write a test that checks to see if our `App` component renders without throwing an error.
+it('App Component Renders Without Error', () => {
+    render(<App />);
+});
+
