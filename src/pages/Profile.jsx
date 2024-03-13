1026852206059ffb4a78cb731e78fc5512e502aa
Dominik Jadczak <123301714+Dominik-Jad@users.noreply.github.com>
2024-03-13 18:57:55 +0000

    added navigatio and router set up

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Profile = () => {
+
+    return (
+        <div>
+            <h2>Profile</h2>
+            <p>Profile page content</p>
+        </div>
+    );
+}
+
+export default Profile;
---
+++ src/pages/Tracker.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Tracker = () => {
+
+    return (
+        <div>
+            <h2>Tracker</h2>
+            <p>Tracker page content</p>
+        </div>
+    );
+}
+
+export default Tracker;
