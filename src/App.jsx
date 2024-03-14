36704c487d84b5f819bf5c739b5fc155fc75d434
Dominik Jadczak <123301714+Dominik-Jad@users.noreply.github.com>
2024-03-14 21:08:03 +0000

    added a login functionality and a way to upload and display resume pdf

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,59 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Tracker from './pages/Tracker';
 import Profile from './pages/Profile';
 import Resources from './pages/Resources';
+import { Auth } from '@supabase/auth-ui-react'
+import { createClient } from '@supabase/supabase-js'
+import { useEffect, useState } from "react";
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+const supabase = createClient(
+  'https://muhmvbrhzksmloawaqcl.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11aG12YnJoemtzbWxvYXdhcWNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAzNjM5MjIsImV4cCI6MjAyNTkzOTkyMn0.3Fr05BJ5aEHumzOU6BBUmtSSwDKxTtcwWo40Jc5qjdw',
+)
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Tracker />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="resources" element={<Resources />} />
-      </Routes>
-    </Router>
-  )
+  const [session, setSession] = useState(null)
+
+  const signout = async () => {
+    setSession(null)
+    await supabase.auth.signOut();
+  }
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (
+      <Auth supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        providers={[]}
+      />
+    )
+  }
+  else {
+    return (
+      <Router>
+        <Navbar signout={signout} />
+        <Routes>
+          <Route path="/" element={<Tracker />} />
+          <Route path="profile" element={<Profile session={session} supabase={supabase} />} />
+          <Route path="resources" element={<Resources />} />
+        </Routes>
+      </Router>
+    )
+  }
 }
+
 
 export default App
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,69 @@
-import React from "react";
+import React from 'react';
+import { useState, useEffect } from "react";
+import { Document, Page } from 'react-pdf';
+import { pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
-const Profile = () => {
+const Profile = (props) => {
+  const [pdfURL, setPdfURL] = useState('');
 
-    return (
-        <div>
-            <h2>Profile</h2>
-            <p>Profile page content</p>
-        </div>
-    );
+  async function uploadResume(e) {
+
+    let file = e.target.files[0];
+
+    const { data, error } = await props.supabase
+      .storage
+      .from('resume')
+      .upload(props.session.user.id + '/' + 'resume.pdf', file)
+
+    if (data) {
+      console.log(data);
+
+    } else {
+      console.log(error);
+    }
+  }
+
+  const getResume = async () => {
+    console.log("get resume");
+    console.log(props.session.user.id + '/' + 'resume.pdf');
+    const { data, error } = await props.supabase
+      .storage
+      .from('resume')
+      .download(props.session.user.id + '/' + 'resume.pdf')
+
+    if (data) {
+      console.log("wow worked")
+      setPdfURL(props.session.user.id + '/' + 'resume.pdf');
+    } else {
+      console.log("wow not worked")
+      console.log(error);
+    }
+  }
+
+
+  return (
+    <div>
+      <h2>Profile</h2>
+      <p>Profile page content</p>
+      <p>testing</p>
+      <p>{props.session.user.id}</p>
+
+      <input type="file" onChange={(e) => uploadResume(e)} />
+      <div>
+        <button onClick={getResume}>Get Resume</button>
+      </div>
+      <div> {
+        pdfURL === '' ? null :
+          <Document file={`https://muhmvbrhzksmloawaqcl.supabase.co/storage/v1/object/public/resume/` + pdfURL}>
+            <Page pageNumber={1} />
+          </Document>
+      }
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
