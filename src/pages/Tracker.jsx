015fb071c9262eb62eee4b8460a07fdd1773895f
Olumide Olayeni <oluyeni@gmail.com>
2024-03-18 11:36:15 +0000

    installed beautiful dnd and created components needed

--- src/pages/Tracker.jsx
+++ src/pages/Tracker.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import {DragDropContext} from'react-beautiful-dnd';
 
 const Tracker = () => {
+    const [Wishlist, setWishlist] = React.useState([]);
+    const [applied, setApplied] = React.useState([]);
+    const [interview, setInterview] = React.useState([]);
+    const [offer, setOffer] = React.useState([]);
+    const [accepted, setAccepted] = React.useState([]);
+
+
 
     return (
-        <div>
-            <h2>Tracker</h2>
-            <p>Tracker page content</p>
-        </div>
+        <DragDropContext>
+            <h3>Job Application Tracker Board</h3>
+        </DragDropContext>
     );
 }
+
 
 export default Tracker;
